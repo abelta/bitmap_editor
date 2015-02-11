020a1f7a9e369acf12ca104fe5919b422733e158
Abel Tamayo <abel.tamayo@gmail.com>
2015-02-11 19:53:03 +0100

    Getting started.

---
+++ bitmap_editor.rb
@@ -0,0 +1,20 @@
+
+class bitmap
+
+  def initialize (height, width)
+    puts "bitmap-initialize"
+
+  end
+
+  def print
+  end
+  
+end
+
+
+def read_input (input)
+  puts "read_input"
+  puts "input #{input}"
+  input = input.split
+  command = input.first
+end
